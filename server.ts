da36563a3ce990ef859c04353590e6e404d6164d
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2023-06-23 10:09:11 +0200

    setup server

--- server.ts
+++ server.ts
@@ -1,3 +1,18 @@
-import { OpenAI } from "langchain/llms/openai";
+import { OpenAIChat } from "langchain/llms/openai";
+import { config } from "dotenv";
 
-console.log({ OpenAI });
+config();
+
+const chat = new OpenAIChat({
+  temperature: 0.9,
+});
+
+console.log("Chatting...");
+chat
+  .call("Hello, how are you?")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
